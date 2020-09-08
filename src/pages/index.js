2237827e60d88e73e2ee36ac4528ed3c16fe8d5b
Yves Riedener <git@reecube.com>
2020-09-08 23:35:43 +0200

    add bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
-import React from "react"
+import React from 'react';
+import './index.scss';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const IndexPage = () => {
+  return (
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">Cool content. Using Bulma!</p>
+        </div>
+        <div className="column is-four-fifths">
+          <h2 className="title is-2">Level 2 heading</h2>
+          <p className="content">This column is cool too!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
